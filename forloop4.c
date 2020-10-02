8b96b5f536096c757a061b281e54a76e76f58baf
baby <snzzare@gmail.com>
2020-10-02 14:57:59 +0700

    .

--- forloop4.c
+++ forloop4.c
@@ -1,31 +1,40 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int r = 3;
     int n = 5;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < i+1; j++)
+        for (int i = 0; i < n; i++)
         {
-            if(j == i){
-                printf(">>");
-            }else{
-                printf("= ");
+            for (int j = 0; j < i + 1; j++)
+            {
+                if (j == i)
+                {
+                    printf(">>");
+                }
+                else
+                {
+                    printf("= ");
+                }
             }
+            printf("\n");
         }
-        printf("\n");
-    }
-    for(int i = n-1 ; i >= 0; i--){
-        for (int j = 0; j < i; j++)
+        for (int i = n - 1; i >= 0; i--)
         {
-            if(j == i-1){
-                printf(">>");
-            }else{
-                printf("= ");
+            for (int j = 0; j < i; j++)
+            {
+                if (j == i - 1)
+                {
+                    printf(">>");
+                }
+                else
+                {
+                    printf("= ");
+                }
             }
+            printf("\n");
         }
-        printf("\n");
-        
     }
-    
     return 0;
 }
